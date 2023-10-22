c9111004c68d0f06a529f179aac6a7241751b2e2
Wiem-Ghars <147325887+Wiem-Ghars@users.noreply.github.com>
2023-10-22 14:19:55 +0100

    Add files via upload

---
+++ WiemGharsLSI3MesureGlycemie/app/src/main/java/com/example/wiem_ghars_lsi3_mesureglycemie/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.wiem_ghars_lsi3_mesureglycemie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
